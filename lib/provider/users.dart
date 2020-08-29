ccd794a4b6eb7a8834b8eda3f38f7bf6faa4f7b7
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-08-28 21:48:38 -0300

    bug de adicionar contato corrigido

--- lib/provider/users.dart
+++ lib/provider/users.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/data/dummy_users.dart';
-import 'package:flutter_crud/models/users.dart';
+import 'package:flutter_crud/models/user.dart';
 
 class Users with ChangeNotifier {
   final Map<String, User> _items = {...DUMMY_USERS};
@@ -15,17 +15,18 @@ class Users with ChangeNotifier {
   User removeByIndex(int i) => _items.remove(i);
 
   void put(User user) {
+    
     if(user == null){
       return;
     }
-    final String id = Random().nextDouble.toString();
+    
+    final String id = Random().nextDouble().toString();
     _items.putIfAbsent(id, () => User(
       id: id,
       name: user.name,
       email: user.email,
       avatarUrl: user.avatarUrl
     ));
-    
     notifyListeners();
   }
   
